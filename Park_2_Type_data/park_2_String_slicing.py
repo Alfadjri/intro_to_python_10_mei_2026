1aa4e3e618311024b02550cad2bc6bc952ee2155
alfadjri28 <alfadjri28@gmail.com>
2026-05-17 13:41:38 +0700

    String Slicing

---
+++ Park_2_Type_data/park_2_String_slicing.py
@@ -0,0 +1,9 @@
+nama_lengkap = "Alfadjri Dwi Fadhilah"
+print("Nama Lengkap : {0}".format(nama_lengkap))
+print("Huruf Pertama (index ke 0): {0}".format(nama_lengkap[0]))
+print("Kata pertama : {0}".format(nama_lengkap[:8]))
+print("Kata Tengah : {0}".format(nama_lengkap[9:12]))
+print("Kata Terakhir : {0}".format(nama_lengkap[13:]))
+print("Duplikat huruf A ada 10 : {0}".format(nama_lengkap[0] * 10))
+print("Nama Anggota : {0}".format(nama_lengkap[13:] + " Anggota"))
+print("tambahakan d : {0}".format(nama_lengkap[13:16] + "d" + nama_lengkap[16:]))
