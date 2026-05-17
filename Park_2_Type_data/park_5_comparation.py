4342cc144d25b7b4c319f18a1e42d430885d6dcc
alfadjri28 <alfadjri28@gmail.com>
2026-05-17 14:14:26 +0700

    comper

---
+++ Park_2_Type_data/park_5_comparation.py
@@ -0,0 +1,27 @@
+a = 10
+b = 10
+# besar dari (>)
+hasil = a > b 
+print(f"Apakah hasil dari {a} lebih besar dari (>) {b} adalah : {hasil}")
+# kecil dari (<)
+hasil = a < b 
+print(f"Apakah hasil dari {a} lebih kecil dari (<) {b} adalah : {hasil}")
+# besar dari sama dengan (>=)
+hasil = a >= b 
+print(f"Apakah hasil dari {a} lebih besar dari sama dengan (>=) {b} adalah : {hasil}")
+# kecil dari sama dengan (<=)
+hasil = a <= b 
+print(f"Apakah hasil dari {a} lebih kecil dari sama dengan (<=) {b} adalah : {hasil}")
+
+c = "1"
+d = 1
+# sama dengan (==)
+hasil = c == d 
+print(f"Apakah hasil dari {c} sama dengan (==) {d} adalah : {hasil}")
+
+f = True
+# not (!=)
+hasil = f != False
+print(f"Apakah hasil dari {f} tidak sama dengan True adalah : {hasil}")
+
+
