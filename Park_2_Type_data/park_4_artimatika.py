43a521dcd7afef0a5eb444c6358ab1fa3bfd402d
alfadjri28 <alfadjri28@gmail.com>
2026-05-17 14:04:13 +0700

    Aritmatika

---
+++ Park_2_Type_data/park_4_artimatika.py
@@ -0,0 +1,33 @@
+x = 9
+y = 2
+
+# Penjumlahan (+)
+hasil = x + y 
+print(f"hasil dari {x} + {y} = {hasil}")
+# Kurang (-)
+hasil = x - y 
+print(f"hasil dari {x} - {y} = {hasil}")
+# Kali (*)
+hasil = x * y 
+print(f"hasil dari {x} * {y} = {hasil}")
+# Bagi (/)
+hasil = x / y 
+hasli_conversi = int(hasil)
+print(f"hasil dari {x} / {y} = {hasil}")
+# Bagi (/)
+hasil = x // y 
+print(f"hasil dari {x} // {y} = {hasil}")
+# Modulus (%) : sisa pembagian
+hasil = x % y 
+print(f"hasil dari {x} % {y} = {hasil}")
+# Pangkat (**)
+hasil = x ** y 
+print(f"hasil dari {x} ** {y} = {hasil}")
+
+
+z = 2000
+# z = z + 1 # tidak baik 
+# incerment
+z += 1
+# decerment
+z -= 1
