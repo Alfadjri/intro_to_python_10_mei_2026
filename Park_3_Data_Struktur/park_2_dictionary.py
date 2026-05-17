952efbbe4d81a8490c9e0b5d9fa457da2b39677b
alfadjri28 <alfadjri28@gmail.com>
2026-05-17 14:56:14 +0700

    dictionary

--- Park_3_Data_Struktur/park_2_dictionary.py
+++ Park_3_Data_Struktur/park_2_dictionary.py
@@ -1,1 +1,19 @@
-profile = {}
+
+# CRUD
+# Create
+list_siswa = {
+    "Kelas" : 12,
+    "Jurusan" : ["Ipa","Ips"],
+    "Nama_Ketua" : "Udin"
+}
+# read
+print(f"Data Mentah : {list_siswa}")
+print(f"Siapa nama ketua kelas : {list_siswa["Nama_Ketua"]}")
+print(f"Kelas ini harus jurusan IPS : {list_siswa["Jurusan"][1]}")
+# Update
+list_siswa["Nama_Ketua"] = "ICA"
+list_siswa["Total"] = 40
+print(f"Data Update : {list_siswa}")
+# Delete
+del list_siswa["Total"]
+print(f"Data Delete : {list_siswa}")
